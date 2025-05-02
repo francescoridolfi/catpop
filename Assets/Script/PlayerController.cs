153b9372f0bd2d5c09efc73350b6f7b456206ea9
Daniele Panzeri <168289711+MrH0pe@users.noreply.github.com>
2025-05-02 15:53:11 +0200

    Aggiunti Walk e Idle Animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,6 +11,11 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 input;
 
+    private Animator animator;
+
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         if (!isMoving)
@@ -18,10 +23,17 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            Debug.Log("This is input.x" + input.x);
+            Debug.Log("This is input.y" + input.y);
+
             if (input.x != 0) input.y = 0;
 
             if (input != Vector2.zero)
             {
+
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
 
                 var targetPos = transform.position;
                 targetPos.x += input.x;
@@ -31,6 +43,8 @@ public class PlayerController : MonoBehaviour
             }
 
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
 
     IEnumerator Move(Vector3 targetPos)
